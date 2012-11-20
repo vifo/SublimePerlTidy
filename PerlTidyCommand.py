7fb4d8ba23aa85ca15e195dec8475578dbfa9009
Victor Foitzik <vifo@cpan.org>
2012-11-20 14:13:31 +0100

    Fix for issue #3 and issue #4. Added debugging messages.
    * Default settings now in DEFAULT_SETTINGS.
    * User will now get a warning, if the command provided in "perltidy_cmd" is
      invalid.
    * When running on Win32, will now search for "perltidy.bat" instead of
      "perltidy", if user did not specify "perltidy_cmd".
    * Changed subprocess handling. Now using Shell=False, when running commands,
      so we won't have to escape arguments, before passing them to perltidy. This
      will most likely fix issue #3, though I am not able to test it.
    * Changed passing input to perltidy/reading output from perltidy. If the data
      to be tidied contains any non-ASCII characters, temporary files will be
      used to pass/read data. This allows us for non-ASCII data to be handled
      correctly. Fixes: issue #4.
    * Added debugging messages. Verbosity level can be set by user in
      "perltidy_log_level".
    * Documentation updated.
    * Reformatted JSON sublime-keymap files.
    * Version bumped to 0.0.3.

--- PerlTidyCommand.py
+++ PerlTidyCommand.py
@@ -3,143 +3,248 @@ import sublime_plugin
 import subprocess
 import os
 import os.path
-import re
-import pipes
+import tempfile
+import codecs
 
 # PerlTidy for Sublime Text 2
 #   rbo@cpan.org
-#
-# TODO:
-#   * Implementing isEnabled
+
+DEFAULT_SETTINGS = {
+    'perltidy_log_level': 0,
+    'perltidy_options': ['-sbl', '-bbt=1', '-pt=2', '-nbbc', '-l=100', '-ole=unix', '-w', '-se'],
+    'perltidy_rc_paths': ['.perltidyrc', 'perltidyrc'],
+    'perltidy_cmd': '/usr/bin/perltidy',
+}
 
 class PerlTidyCommand(sublime_plugin.TextCommand):
 
-    _perltidy_rc_paths = None
-    _perltidy_default_rc_paths = ['.perltidyrc', 'perltidyrc']
     _perltidy_cmd = None
-    _perltidy_default_cmd = '/usr/bin/perltidy'
+    _perltidy_log_level = None
     _perltidy_options = None
-    _perltidy_default_options = ['-sbl', '-bbt=1', '-pt=2', '-nbbc', '-l=100', '-ole=unix', '-w', '-se']
+    _perltidy_rc_paths = None
 
     def run(self, edit):
+        self.load_settings()
+
+        # Bailout, if we don't have a valid perltidy command to run.
         cmd = self.get_perltidy_cmd()
         if cmd is None:
             sublime.error_message(
-                'PerlTidy error: Cannot find perltidy in PATH. Please setup your environment ' +
-                'variable PATH, so it contains perltidy, or specify perltidy location in user ' +
-                'setting "perltidy_cmd".')
+                'PerlTidy: Cannot find perltidy in any directory given in environment variable ' +
+                'PATH. Please setup your environment variable PATH, so it contains perltidy, ' +
+                'or specify perltidy location in user setting "perltidy_cmd". Please refer ' +
+                'to documentation at https://github.com/rbo/st2-perltidy for details.')
             return
 
-        selection = 0
-        for r in self.view.sel():
-            if not r.empty():
-                selection += 1
-                self.tidy_region(edit, r)
+        # Check, if we have any non-empty selections and tidy them.
+        regions_tidied = 0
+        for region in self.view.sel():
+            if not region.empty():
+                regions_tidied += 1
+                self.tidy_region(edit, region)
 
-        if selection == 0:
-            cursor = self.view.sel()[0]
+        # If no selections have been tidied so far, go ahead and tidy entire
+        # view. Reposition cursor after tidying up.
+        if regions_tidied == 0:
+            cursor_pos = self.view.sel()[0]
             if self.tidy_region(edit, sublime.Region(0L, self.view.size())):
-                if cursor.empty():
-                    self.view.sel().add(cursor)
-                    self.view.sel().subtract(self.view.sel()[1])
+                if cursor_pos.empty():
+                    self.view.sel().add(cursor_pos)
+                    if len(self.view.sel()) > 1:
+                        self.view.sel().subtract(self.view.sel()[1])
                     self.view.show_at_center(self.view.sel()[0].begin())
 
+    def pp(self, string):
+        result = []
+
+        if type(string) is list:
+            for i in string:
+                result.append('"' + i + '"')
+        else:
+            result.append('"' + string + '"')
+
+        return ' '.join(result)
+
+    def load_settings(self):
+        if self._perltidy_log_level is None:
+            self._perltidy_log_level = self.view.settings().get('perltidy_log_level', DEFAULT_SETTINGS['perltidy_log_level'])
+        if self._perltidy_options is None:
+            self._perltidy_options = self.view.settings().get('perltidy_options', DEFAULT_SETTINGS['perltidy_options'])
+        if self._perltidy_rc_paths is None:
+            self._perltidy_rc_paths = self.view.settings().get('perltidy_rc_paths', DEFAULT_SETTINGS['perltidy_rc_paths'])
+
     def get_perltidy_cmd(self):
-        # if not already done, load perltidy command from settings, or
-        # fallback to default cmd defined above
+
+        # If not already done, load perltidy command from settings, or fall
+        # back to default command defined in class above.
         if self._perltidy_cmd is None:
             cmd = self.view.settings().get('perltidy_cmd')
+            user_provided_perltidy_cmd = True
+
             if cmd is None:
-                cmd = self._perltidy_default_cmd
+                cmd = DEFAULTS['perltidy_cmd']
+                user_provided_perltidy_cmd = False
+
             if type(cmd) is not list:
                 cmd = [cmd]
 
-            # first element of self._perltidy_cmd must be a file, if not
-            # assume that the command is invalid and try to find perltidy
-            # within path
+            # First element of self._perltidy_cmd must be (an executable)
+            # file. If not assume that the command is invalid and try to find
+            # perltidy within path. Emit warning, if the command is invalid,
+            # and has been provided by the user.
             if not os.path.isfile(cmd[0]):
+                if user_provided_perltidy_cmd:
+                    print 'PerlTidy: Command {0} specified in user setting "perltidy_cmd" could not be found. Ignoring and searching perltidy in PATH.'.format(self.pp(cmd[0]))
                 cmd = None
-                for path in os.environ["PATH"].split(os.pathsep):
-                    path_cmd = os.path.join(path, 'perltidy')
 
-                    #print "PerlTidy: Checking for perltidy in " + path_cmd
-                    if os.path.isfile(path_cmd):
-                        cmd = [path_cmd]
+                # Search for perltidy (or perltidy.bat on Win32) within PATH.
+                perltidy_filename = 'perltidy'
+                if os.name == 'nt':
+                    perltidy_filename += '.bat'
+
+                for path in os.environ["PATH"].split(os.pathsep):
+                    cmd_path = os.path.join(path, perltidy_filename)
+                    if self._perltidy_log_level >= 2:
+                        print 'PerlTidy: Checking for perltidy: ' + self.pp(cmd_path)
+
+                    if os.path.isfile(cmd_path):
+                        if self._perltidy_log_level >= 1:
+                            print 'PerlTidy: Using perltidy: ' + self.pp(cmd_path)
+                        cmd = [cmd_path]
                         break
 
+            # Save cmd for later usage.
             if cmd is not None:
+                if self._perltidy_log_level >= 2:
+                    print 'PerlTidy: Using perltidy command: ' + self.pp(cmd)
                 self._perltidy_cmd = cmd
 
         return self._perltidy_cmd
 
+    # Return list containing perltidy options as defined in user setting
+    # "perltidy_options" or DEFAULTS['perltidy_options']
     def get_perltidy_options(self):
-        if self._perltidy_options is None:
-            self._perltidy_options = self.view.settings().get('perltidy_options')
-            if self._perltidy_options is None:
-                self._perltidy_options = self._perltidy_default_options
-
         return self._perltidy_options
 
-    # return paths for perltidy configuration files; load them either from
-    # setting "perltidy_rc_paths", or fallback to self._perltidy_default_rc_paths,
-    # if none specified by user
+    # Return list containing possible perltidyrc paths as defined in user
+    # setting "perltidy_rc_paths" or DEFAULTS['perltidy_rc_paths']
     def get_perltidy_rc_paths(self):
-        if self._perltidy_rc_paths is None:
-            self._perltidy_rc_paths = self.view.settings().get('perltidy_rc_paths')
-            if self._perltidy_rc_paths is None:
-                self._perltidy_rc_paths = self._perltidy_default_rc_paths
-
         return self._perltidy_rc_paths
 
-    # tidy given region; returns True on success or False on perltidy runtime
-    # error.
-    def tidy_region(self, edit, region):
-
-        # build command
-        cmd = []
-        cmd.extend(self.get_perltidy_cmd())
-        cmd.extend(self.get_perltidy_options())
-
-        # try to find a tidyrc file, break on first tidyrc file found
+    # Search for perltidyrc file in current project, based on possible file
+    # paths configured in user setting "perltidy_rc_paths". Return first
+    # perltidyrc path found, or None, if the project does not contain any
+    # perltidyrc files.
+    def get_perltidy_rc_path(self):
         try:
             for folder in self.view.window().folders():
-                for tidyrc_path in self.get_perltidy_rc_paths():
-                    tidyrc_full_path = tidyrc_path if os.path.isabs(tidyrc_path) else os.path.join(folder, tidyrc_path)
+                for perltidy_rc_path in self.get_perltidy_rc_paths():
 
-                    # does this tidyrc file exist?
-                    if os.path.isfile(tidyrc_full_path):
+                    # Construct absolute path, if not absolute yet.
+                    perltidy_rc_path = perltidy_rc_path if os.path.isabs(perltidy_rc_path) else os.path.join(folder, perltidy_rc_path)
 
-                        # on Win32, replace backslashes with forward slashed in
-                        # perltidy.rc filepath, otherwise perltidy is unable to find
-                        # the tidyrc file
-                        if os.name.lower() == 'nt':
-                            tidyrc_full_path = re.sub(r'\\', '/', tidyrc_full_path)
+                    if self._perltidy_log_level >= 2:
+                        print 'PerlTidy: Checking for perltidyrc: ' + self.pp(perltidy_rc_path)
 
-                        cmd.append('-pro={0}'.format(pipes.quote(tidyrc_full_path)))
+                    # Does this perltidyrc file exist?
+                    if os.path.isfile(perltidy_rc_path):
+                        if self._perltidy_log_level >= 2:
+                            print 'PerlTidy: Using perltidyrc: ' + self.pp(perltidy_rc_path)
                         raise StopIteration()
+                    else:
+                        perltidy_rc_path = None
 
         except StopIteration:
             pass
 
-        p = subprocess.Popen(
-            cmd,
-            bufsize = -1,
-            shell   = True,
-            stdout  = subprocess.PIPE,
-            stderr  = subprocess.PIPE,
-            stdin   = subprocess.PIPE)
+        if perltidy_rc_path is None and self._perltidy_log_level >= 2:
+            print 'PerlTidy: No perltidyrc found in project'
 
-        #print 'PerlTidy: running command: ' + ' '.join(cmd)
+        return perltidy_rc_path
 
-        output, error = p.communicate(self.view.substr(region))
+    # Tidy given region; returns True on success or False on perltidy runtime
+    # error.
+    def tidy_region(self, edit, region):
 
+        # Build command.
+        cmd = []
+        cmd.extend(self.get_perltidy_cmd())
+        cmd.extend(self.get_perltidy_options())
+
+        # Check, if we have a perltidyrc in the current project and append to
+        # command.
+        perltidy_rc_path = self.get_perltidy_rc_path()
+        if perltidy_rc_path is not None:
+            cmd.append('-pro=' + perltidy_rc_path)
+
+        # Prepare arguments for subprocess call.
+        subprocess_args = {
+            'bufsize': -1,
+            'shell': False,
+            'stdout': subprocess.PIPE,
+            'stderr': subprocess.PIPE,
+            'stdin': subprocess.PIPE
+        }
+
+        # Hide console window on Win32.
+        if os.name == 'nt':
+            subprocess_args['startupinfo'] = subprocess.STARTUPINFO()
+            subprocess_args['startupinfo'].dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        # Check, if the data to be tidied has any non-ASCII characters. If
+        # yes, prepare temporary files for input and output with UTF-8
+        # encoding, and spool the input to file. Adjust perltidy call, so data
+        # will be read and written to temporary files.
+        input = self.view.substr(region)
+        use_temporary_files = False
+
+        try:
+            input.decode('ascii')
+        except UnicodeEncodeError:
+            use_temporary_files = True
+
+        if use_temporary_files:
+            # Create temporary files for input/output and reopen them with
+            # codecs.open, so we can specify an encoding for the files. At
+            # least with Python 2.6, there seems to be no other option.
+            subprocess_args['stdout'] = None
+            subprocess_args['stdin'] = None
+
+            perltidy_input_fh, perltidy_input_filepath = tempfile.mkstemp()
+            perltidy_output_fh, perltidy_output_filepath = tempfile.mkstemp()
+            os.close(perltidy_input_fh)
+            os.close(perltidy_output_fh)
+
+            with codecs.open(perltidy_input_filepath, 'w+b', encoding='utf-8') as fh:
+                fh.write(input)
+
+            cmd.append(perltidy_input_filepath)
+            cmd.append('-o=' + perltidy_output_filepath)
+            input = None
+
+        if self._perltidy_log_level >= 1:
+            print 'PerlTidy: Running command: ' + ' '.join(cmd)
+
+        p = subprocess.Popen(cmd, **subprocess_args)
+        output, error = p.communicate(input)
+
+        # If we're using temporary files for I/O, load output from output file
+        # and cleanup temporary files.
+        if use_temporary_files:
+            with codecs.open(perltidy_output_filepath, 'rb', encoding='utf-8') as fh:
+                output = fh.read();
+            os.unlink(perltidy_input_filepath)
+            os.unlink(perltidy_output_filepath)
+
+        # Replace region with output, if we had no errors. Otherwise create a
+        # scratch window with perltidy error output.
         if not error:
             self.view.replace(edit, region, output)
             return True
         else:
             results = self.view.window().new_file()
             results.set_scratch(True)
-            results.set_name('PerlTidy error output')
+            results.set_name('PerlTidy: Error output')
             edit = results.begin_edit()
             results.insert(edit, 0, error)
             results.end_edit(edit)
--- README.markdown
+++ README.markdown
@@ -1,8 +1,12 @@
 # PerlTidy for Sublime Text 2
 
+PerlTidy is a plugin for [Sublime Text 2](http://www.sublimetext.com/), which integrates [perltidy](http://perltidy.sourceforge.net/) into ST2. It indents and reformats Perl scripts to make them easier to read.
+
 ## Installation
 
-**With Sublime Text 2 Package Control:** Open *Package Control: Install Package*, search for "PerlTidy" and install.
+**With Sublime Package Control:**  The easiest way to install PerlTidy is through [Sublime Package Control](http://wbond.net/sublime_packages/package_control).
+
+Once you have installed Package Control, restart Sublime Text 2 and bring up the Command Palette (press `Control+Shift+P` on Linux/Windows, `Command+Shift+P` on OS X, or select `Tools->Command Palette...` from menu). Select "Package Control: Install Package", wait till Package Control fetches the latest package list, then select "PerlTidy" from the list of available packages. The advantage of using this method is, that Package Control will automatically keep PerlTidy up to date with the latest version.
 
 **Without Git:** Download the latest source from [GitHub](https://github.com/rbo/st2-perltidy/downloads) and copy the folder *st2-perltidy* to your Sublime Text *Packages* directory.
 
@@ -28,31 +32,36 @@ The *Packages* directory is located at:
 
 ### Key bindings
 
-Defaults to *ctrl+shift+t* for Windows / Linux and *super+shift+t* for OSX, since *ctrl+t* is used by some other plugins. Change in *Sublime Text 2 => Preferences => Key Bindings - Default* or *Key Bindings - User*.
+Defaults to `Ctrl+Shift+t` for Windows/Linux and `Super+Shift+t` for OS X, since `Ctrl+t` is used by some other plugins. Change in `Preferences->Key Bindings->Default` or `Preferences->Key Bindings - User`.
 
 ### Settings
 
-Open *Sublime Text 2 -> Preferences -> Settings - Default* or *Settings - User*. Add the following lines:
+Open `Preferences->Settings - Default` or `Preferences->Settings - User`. Add the following lines:
 
     // Specify full path to perltidy and optionally the Perl interpreter. If
     // not specified, will search PATH for perltidy.
     //"perltidy_cmd": "/opt/perl/bin/perltidy"
     //"perltidy_cmd": [ "C:\\strawberry\\perl\\bin\\perl.exe", "C:\\strawberry\\perl\\site\\bin\\perltidy" ]
 
-    // Specify what perltidy profiles to search for within current project. Note, that
-    // only the first matching profile within the project will be used. Absolute paths
-    // may also be used.
+    // Specify what perltidyrc files to search for within current project. Note,
+    // that only the first matching profile within the project will be used.
+    // Absolute paths may also be used.
     //"perltidy_rc_paths": [ "perltidy.rc", ".perltidy.rc" ]
 
     // Specify perltidy options. Defaults to [ "-sbl", "-bbt=1", "-pt=2", "-nbbc", "-l=100", "-ole=unix", "-w", "-se" ]
     // if not given.
     //"perltidy_options": [ "-sbl", "-bbt=1", "-pt=2", "-nbbc", "-l=100", "-ole=unix", "-w", "-se" ]
 
+    // Log level for perltidy operations. Set to 1 to enable informational
+    // messages and to 2 for full debugging. Defaults to 0, so only warnings
+    // and errors will be displayed on the console.
+    //"perltidy_log_level": 0
+
 Note that all of the above settings may be overridden per project. In order to do this, add the above lines to your .sublime-project file.
 
 ### Default perltidy options
 
-The default perltidy options are set as follows (you may override them by setting *perltidy_options* in your user preferences):
+The default perltidy options are set as follows (you may override them by setting "perltidy_options" in your user preferences):
 
 * -sbl
 
@@ -84,12 +93,19 @@ The default perltidy options are set as follows (you may override them by settin
 
 * -se
 
-  Errors go to STDERR. Please ensure, that you include this settings, when changing default options. Otherwise, perltidy error messages won't appear in a separate window. Details: [perltidy Documentation | Standard Error Output](http://perltidy.sourceforge.net/perltidy.html#se_standard_error_output).
+  Errors go to STDERR. **Please ensure, that you include this settings, when changing default options.** Otherwise, perltidy error messages won't appear in a separate window. Details: [perltidy Documentation | Standard Error Output](http://perltidy.sourceforge.net/perltidy.html#se_standard_error_output).
+
+## Troubleshooting
+
+During normal operation, PerlTidy will emit warnings and errors to the Sublime Text 2 console (open with ``Ctrl+` ``). In order to enable additional diagnostic messages, adjust user setting "perltidy_log_level" as follows:
+
+* 0 == Warnings and error messages only. This is the default.
+
+* 1 == Print system commands used for tidying up content and perltidyrc file paths used (if any).
+
+* 2 == Full debugging. In addition to the above, print where PerlTidy searches for perltidy and/or perltidyrc.
 
 ## TODOs:
 
 * Implement isEnabled
-
-## Known issues:
-
-* On Win32, files containing UTF-8 characters (more specifically: characters that are not within the currently configured Win32 console character map), will not be formatted properly. This is a limitation of the Win32 console I/O / Python [subprocess](http://docs.python.org/2/library/subprocess.html) combo, and may be fixed in the future with temporary files (See [Issue #4](https://github.com/rbo/st2-perltidy/issues/4)).
+* Implement automatic tidying upon save.
